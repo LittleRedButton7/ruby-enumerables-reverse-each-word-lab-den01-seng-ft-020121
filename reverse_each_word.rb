549b020b8a1c24fdafef2fdfe332239207652439
Marissa Nolan <ritz1484@cs.com>
2021-01-21 00:22:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,7 @@ def reverse_each_word(string)
  #take each word in sentence of strings and turn into an Array
  #use the .each method to reverse the letters in each Array
  #return the sentence with each reversed word
+ string.split(" ")
  
 
 end
