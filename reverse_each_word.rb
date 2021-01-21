f7f0703f34e563d996413000fac6448435428289
Marissa Nolan <ritz1484@cs.com>
2021-01-21 00:28:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,10 +4,8 @@ def reverse_each_word(string)
  #take each word in sentence of strings and turn into an Array
  #use the .each method to reverse the letters in each Array
  #return the sentence with each reversed word
- array = string.split(" ")
+ array = string.split(" ,")
  array.collect do |word|
    word.reverse
  end
 end 
-
-end
