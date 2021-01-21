95382de117b429cc9ffc9a87cfd81f1db22c5f0d
Marissa Nolan <ritz1484@cs.com>
2021-01-21 00:31:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 def reverse_each_word(string)
  #take each word in sentence of strings and turn into an Array
- #use the .each method to reverse the letters in each Array
+ #use the .collect method to reverse the letters in each Array
  #return the sentence with each reversed word
  array = string.split(" ")
  array.collect do |word|
-   word.reverse
+   word.str.reverse
  end
 end 
